55b7adc13d8a34ffb6604df48717f8f2370cbf5e
Toby Brancher <toby@torchboxs-mbp-5.dhc.itl.tbbrs.torchbox.net>
2016-10-19 15:43:10 +0100

    first commit

---
+++ Toby_Node.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace PatternLab\PatternEngine\Twig;
+
+class Toby_Node extends Twig_Node
+{
+    public function __construct($name, Twig_Node_Expression $value, $line, $tag = null)
+    {
+        parent::__construct(array('value' => $value), array('name' => $name), $line, $tag);
+    }
+
+    public function compile(Twig_Compiler $compiler)
+    {
+        $compiler
+            ->addDebugInfo($this)
+            ->write('$context[\''.$this->getAttribute('name').'\'] = ')
+            ->subcompile($this->getNode('value'))
+            ->raw(";\n")
+        ;
+    }
+}
